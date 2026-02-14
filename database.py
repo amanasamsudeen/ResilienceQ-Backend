cb6cd874b55dcabc710d77fad035d1b7e45e419a
Amana Samsudeen <amanasamsudeen@gmail.com>
2026-02-14 12:47:10 +0530

    database.py changed

--- database.py
+++ database.py
@@ -1,14 +1,30 @@
-from sqlalchemy import create_engine;
-from dotenv import load_dotenv;
-import os;
-from sqlalchemy.orm import sessionmaker,  declarative_base;
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker( autoflush=False, bind=engine)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables!")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"},
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
